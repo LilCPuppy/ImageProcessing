439f9f10dc08bcf7c0350bad6d7a853632dd85e4
briancknight <briacknight31@gmail.com>
2019-08-29 10:55:00 -0700

    merging

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/brknight/Documents/ImageProcessing/source/Noise.cpp" "/Users/brknight/Documents/ImageProcessing/build/CMakeFiles/main.dir/source/Noise.cpp.o"
+  "/Users/brknight/Documents/ImageProcessing/source/Segmentation.cpp" "/Users/brknight/Documents/ImageProcessing/build/CMakeFiles/main.dir/source/Segmentation.cpp.o"
+  "/Users/brknight/Documents/ImageProcessing/source/Tools.cpp" "/Users/brknight/Documents/ImageProcessing/build/CMakeFiles/main.dir/source/Tools.cpp.o"
+  "/Users/brknight/Documents/ImageProcessing/source/main.cpp" "/Users/brknight/Documents/ImageProcessing/build/CMakeFiles/main.dir/source/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
