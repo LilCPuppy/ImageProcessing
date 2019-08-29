7f128ddb2d1964bc80f08d9ab03224a1d0764a9b
LilCPuppy <ccarro07@calpoly.edu>
2019-08-29 08:16:55 -0700

    hello, world

---
+++ source/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <memory>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+//#include "Noise.h"
+//#include "Demos.h"
+//#include "Transforms.h"
+//#include "Segmentation.h"
+//#include "Multiscale.h"
+//#include "Metric.h"
+
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+
+    printf("Hello, world\n");
+
+    return 1;
+}
+
